3adf36f5f9f1d06711c6e7e66650dba36115d742
vietkhang <doanvankhoa2011@gmail.com>
2025-12-13 08:59:33 +0700

    commit lại những gì Khải commit bị lỗi

---
+++ HumanResourcesManagementSystemFinal/Converters/UpperCaseConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace HumanResourcesManagementSystemFinal.Converters
+{
+    public class UpperCaseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string input)
+            {
+                return input.ToUpper(culture);
+            }
+            return value;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}

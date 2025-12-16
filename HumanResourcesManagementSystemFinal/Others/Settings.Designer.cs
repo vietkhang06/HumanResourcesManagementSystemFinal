cee1889cd77cf121c58a89c61dc383b269828fa0
ja-gnahk <24520765@gm.uit.edu.vn>
2025-12-16 22:03:18 +0700

    CapNhatCode  final 16/12/2025

--- HumanResourcesManagementSystemFinal/Others/Settings.Designer.cs
+++ HumanResourcesManagementSystemFinal/Others/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace HumanResourcesManagementSystemFinal {
+namespace HumanResourcesManagementSystemFinal.Others {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
